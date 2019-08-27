278fe594ee0c85aef574f5017fd376e6a5826afc
kasa2001 <pawelgomolka@interia.pl>
2019-08-27 14:13:04 +0200

    Remove Test Classes

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("APPLICATION", __DIR__ . "/..");
-define("APPLICATION_SRC", APPLICATION . "/src");
-define('APPLICATION_PATH', __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR);
-
-include APPLICATION . "/vendor/autoload.php";
